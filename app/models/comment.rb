da79dade40f66e9a4ac7f5bd742916bc20931eb4
Derrick1451 <haylederrick14@gmail.com>
2023-08-31 21:13:50 +0300

    Edit model files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,7 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    def comments_counter
-        post.increament!(:comments_counter)
-        
-    end
-
+  belongs_to :user
+  belongs_to :post
+  def comments_counter
+    post.increament!(:comments_counter)
+  end
 end
