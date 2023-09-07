125417d1fcff356a6fce93e55521835d1d176ebf
Derrick1451 <haylederrick14@gmail.com>
2023-09-07 22:32:01 +0300

    Update controller files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @user_id = User.find(params[:id])
+  end
 end
